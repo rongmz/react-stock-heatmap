14c9d6cf0a9996822c31e9a8693d85eabf53f983
rongmz <rounak.saha0507@gmail.com>
2020-09-18 02:13:30 +0530

    Updates

---
+++ precache-manifest.33c6cff2fac03da60251df264d3bf1ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec482ac4dd6c3b4cff5174884b7365fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ce6e84773bf7cda51d7b",
+    "url": "./static/css/main.e499b826.chunk.css"
+  },
+  {
+    "revision": "e701841bc401538b0777",
+    "url": "./static/js/2.c4a4bbd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c4a4bbd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce6e84773bf7cda51d7b",
+    "url": "./static/js/main.e6e42da9.chunk.js"
+  },
+  {
+    "revision": "82c2a7af8674ee91f33b",
+    "url": "./static/js/runtime-main.a8c32e34.js"
+  }
+]);
